b433ba2f13c3f8f885d4b0cd8c2ba11c66481ffa
JowisG <j.juegos0210@gmail.com>
2025-02-09 17:56:12 +0100

    Init: Version 0

---
+++ Simulador_Trafico_v0/src/simulator/model/MoveFirstStrategy.java
@@ -0,0 +1,15 @@
+package simulator.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveFirstStrategy implements DequeuingStrategy {
+
+	@Override
+	public List<Vehicle> dequeue(List<Vehicle> q) {
+		List<Vehicle> v = new ArrayList<>();
+		v.add(q.get(0)); // No copio el objeto vehicle
+		return v;
+	}
+
+}
