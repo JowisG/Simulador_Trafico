2104c114f21cda4ea45d655dac69f01915f76aec
JowisG <j.juegos0210@gmail.com>
2025-02-16 16:34:04 +0100

    Version 1 (falta main)

---
+++ Simulador_Trafico_v1/src/simulator/control/Controller.java
@@ -0,0 +1,58 @@
+package simulator.control;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import simulator.factories.Factory;
+import simulator.model.Event;
+import simulator.model.TrafficSimulator;
+
+public class Controller {
+	/* Atributos */
+	private TrafficSimulator simulator;
+	private Factory<Event> event_factory;
+	
+	// Constructora publica
+	public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
+		if (sim == null)
+			throw new IllegalArgumentException("The TrafficSimulator is null");
+		else
+			simulator = sim;
+		if(eventsFactory == null)
+			throw new IllegalArgumentException("The event factory is null");
+		else
+			event_factory = eventsFactory;
+	}
+	
+	// Metodos
+	public  void loadEvents(InputStream in) {
+		// Convierte la entrada en un JSONObject pra manejarlo
+		JSONObject json = new JSONObject(new JSONTokener(in));
+		// Transformo el json a un array de json para que pueda extraer los eventos y pasarlos por la factoria
+		if(json.has("events")) {
+			JSONArray json_events = json.getJSONArray("events");
+			for(int i = 0; i < json_events.length(); i++) {
+				Event e = event_factory.create_instance(json_events.getJSONObject(i));
+				simulator.addEvent(e);
+			}
+		}else
+			throw new IllegalArgumentException("The Input given does not have the required format");
+	}
+	
+	public void run(int n, OutputStream out) {
+		JSONArray json = new JSONArray();
+		for(int i = 0; i < n; i++) {
+			simulator.advance();
+			json.put(simulator.report());
+		}
+	}
+	
+	public void reset() {
+		simulator.reset();
+	}
+	
+}
