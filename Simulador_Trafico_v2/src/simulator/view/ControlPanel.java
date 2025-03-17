9e086361f1e2e82709596b1571c16ccd1a72766e
JowisG <j.juegos0210@gmail.com>
2025-03-17 23:40:55 +0100

    Añadido JFileChooser

--- Simulador_Trafico_v2/src/simulator/view/ControlPanel.java
+++ Simulador_Trafico_v2/src/simulator/view/ControlPanel.java
@@ -4,16 +4,24 @@ import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Collection;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JSpinner;
 import javax.swing.JToolBar;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.ImageIcon;
 
 import simulator.control.Controller;
@@ -40,19 +48,20 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		// Añadimos los botones
 		JButton file_btn = new JButton();
 		file_btn.setIcon(new ImageIcon("resources/icons/open.png"));
+		file_btn.addActionListener(e -> {
+			load_file();
+		});
 		bar.add(file_btn);
 		
-		bar.addSeparator();
-		JSeparator sep_file = new JSeparator();
-		sep_file.setOrientation(JSeparator.VERTICAL);
-		bar.add(sep_file);
-		bar.addSeparator();
+		separator(bar);
 		
 		JButton co2_btn = new JButton();		co2_btn.setIcon(new ImageIcon("resources/icons/co2class.png"));
 		bar.add(co2_btn);
 		
 		JButton weather_btn = new JButton();		weather_btn.setIcon(new ImageIcon("resources/icons/weather.png"));
 		bar.add(weather_btn);
+		
+		separator(bar);
 		
 		JButton run_btn = new JButton();		run_btn.setIcon(new ImageIcon("resources/icons/run.png"));
 		bar.add(run_btn);
@@ -68,13 +77,9 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		bar.add(ticks_label);
 		bar.add(ticks);
 		
-		bar.addSeparator();
 		// Creating a "Glue" sp that quit goes at the right
 		bar.add(Box.createHorizontalGlue());
-		JSeparator sep_end = new JSeparator();
-		sep_end.setOrientation(JSeparator.VERTICAL);
-		bar.add(sep_end);
-		bar.addSeparator();
+		separator(bar);
 		
 		JButton quit_btn = new JButton();
 		quit_btn.setIcon(new ImageIcon("resources/icons/exit.png"));
@@ -83,6 +88,31 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		// Añadimos el JToolBar al ControlPanel
 		this.add(bar);
 		this.setVisible(true);
+	}
+	
+	private void load_file() {
+		JFileChooser chooser = new JFileChooser("./resources");
+		chooser.setFileFilter(new FileNameExtensionFilter("JSON file", "json"));
+		if(chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+			try {
+				// TODO mejorar esto
+				InputStream input = new FileInputStream(chooser.getSelectedFile());
+				_ctrl.reset();
+				_ctrl.loadEvents(input);
+				_ctrl.run(0);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void separator(JToolBar bar) {
+		bar.addSeparator();
+		JSeparator sep_file = new JSeparator();
+		sep_file.setOrientation(JSeparator.VERTICAL);
+		bar.add(sep_file);
+		bar.addSeparator();
 	}
 
 	@Override
